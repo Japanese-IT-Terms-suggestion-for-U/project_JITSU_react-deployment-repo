11bab62c8320c6142749abec5e29435121c71df1
devYuMinKim <gimyumin40@gmail.com>
2023-10-14 18:23:07 +0900

    💄 style: Implement footer style

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="sm:absolute bottom-0 w-full flex justify-center items-center p-6 z-10">
+  <div class="text-center">
+    &copy; 2023 <a href="https://github.com/yuminn-k/project_JITSU-deployment-repo">JITSU</a>. All rights reserved.
+  </div>
+</footer>
