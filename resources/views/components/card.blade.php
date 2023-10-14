9056f40c5428b4c69343204c808682a29f05a487
devYuMinKim <gimyumin40@gmail.com>
2023-10-14 18:23:43 +0900

    💄 style: Implement card layout

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,41 @@
+<div class="relative mt-6 flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+<div class="p-6">
+    <h5 class="mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased text-center">
+    단어 예시
+    </h5>
+    <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased text-center">
+    뜻 예시
+    </p>
+</div>
+<div class="p-6 pt-0 flex justify-center">
+    <a href="#">
+    <button
+        type="button"
+        data-md-button
+        data-md-color-class="
+        'flex select-none items-center gap-
+        'rounded-lg py-
+        'text-center align-middle 
+        'font-sans text-xs font-bold uppercase 
+        'text-pink transition-all hover:bg-pink 
+        '/10 active:bg-pink/30 disabled:pointer-events-none disabled:opacity-disabled:shadow-none'
+        "
+    >
+        더보기
+        <svg xmlns= "http://www.w3.org/2000/svg"
+            fill= "none"
+            viewBox= "0, ,24, ,24"
+            stroke-width= "2"
+            stroke= "currentColor"
+            aria-hidden= "true"
+            class= "h-,4 w-,4">
+        <path stroke-linecap= "round"
+                stroke-linejoin = "round",
+                d = ",M17,25,8,.25L21,.25ZM17,.25H3"/>
+        </svg>
+    </button>
+    </a>
+</div>
+</div>
+
+<link rel = stylesheet href ="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css"/>
