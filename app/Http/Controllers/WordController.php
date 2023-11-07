e8458e619105c5cbd3e490955ad8a15b7b80777d
devYuMinKim <gimyumin40@gmail.com>
2023-11-07 20:12:40 +0900

    ✨ feat: Add WordController and update card component

---
+++ app/Http/Controllers/WordController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Word;
+use App\Models\Tag;
+
+class WordController extends Controller
+{
+    public function store(Request $request)
+    {
+        $word = new Word([
+            'japanese' => $request->japanese,
+            'korean' => $request->korean,
+            'korean_definition' => $request->korean_definition,
+            'tag' => $request->tag,
+        ]);
+
+        $word->save();
+
+        $tagExists = Tag::where('tag_name', $request->tag)->exists();
+
+        if (!$tagExists) {
+            $tag = new Tag(['tag_name' => $request->tag]);
+            $tag->save();
+        }
+
+        return redirect('dashboard');
+    }
+
+    public function index()
+    {
+        $word = Word::first();
+        return view('dashboard', ['word' => $word]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WordController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,11 +17,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('main');
-});
+})->name('main');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboard', [WordController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::get('/board', function () {
+    return view('board');
+})->middleware(['auth', 'verified'])->name('board');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
