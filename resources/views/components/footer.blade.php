fecd563042ba93f1db0a77aa988c126b237e4aae
devYuMinKim <gimyumin40@gmail.com>
2023-11-12 16:45:10 +0900

    💄 style: Update UI with new components and images

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="sm:absolute bottom-0 w-full flex justify-center items-center p-6 z-10">
-  <div class="text-center">
-    &copy; 2023 <a href="https://github.com/yuminn-k/project_JITSU-deployment-repo">JITSU</a>. All rights reserved.
+<footer class="relative bottom-0 w-full flex justify-center items-center p-6 z-10 bg-white dark:bg-gray-800 border-t border-gray-100 dark:border-gray-700">
+  <div class="text-center text-gray-800 dark:text-gray-200">
+    &copy; 2023 <a href="https://github.com/yuminn-k/project_JITSU-deployment-repo" class="text-blue-500 hover:text-blue-700">JITSU</a>. All rights reserved.
   </div>
 </footer>
