58048ca0eda0c39dcd96f6ef832bb49b67f355e8
devYuMinKim <gimyumin40@gmail.com>
2023-12-06 02:58:39 +0900

    🐛 fix: Add CommentPolicy and register it in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Models\Comment;
+use App\Policies\CommentPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +23,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Gate::policy(Comment::class, CommentPolicy::class);
     }
 }
