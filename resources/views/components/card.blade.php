00e344bf2698cc86374f31476361e0ca25f4e1cc
ユミン <55650732+yuminn-k@users.noreply.github.com>
2023-12-13 05:06:27 +0900

    💄 style: Add tag card.blade.php and dashboard.blade.php (#86)

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -25,11 +25,14 @@
 <div class="relative mt-6 w-96 rounded-xl overflow-hidden shadow-md">
     <div id="card" class="relative w-full h-64 transform-gpu transition-transform duration-1000 ease-in-out">
         <div id="cardFront" class="absolute w-full h-full bg-white flex flex-col p-6 items-center justify-center">
-            <h5 class="mb-2 font-sans text-xl font-semibold text-black text-center">
-                {{ $word ? $word->japanese : '기본값' }}
-            </h5>                
-            <p class="font-sans text-lg font-light text-black text-center">
-                {{ $word ? $word->korean : '기본값' }}
+            <h5 class="mb-2 font-sans text-2xl font-semibold text-black text-center">
+                {{ $word ? $word->japanese : '用語ディフォルト' }}
+            </h5>
+            <p id="tag" class="tag font-sans text-sm font-light text-black text-center">
+                {{ $word->tag ? $word->tag->tag_name : 'タッグディフォルト' }}
+            </p>
+            <p id="korean" class="korean font-sans text-xl font-light text-black text-center">
+                {{ $word ? $word->korean : '意味ディフォルト' }}
             </p>
             <button id="flipButton" class="mt-6 px-4 py-2 bg-pink-500 text-white rounded-lg">
                 詳しく
@@ -37,7 +40,7 @@
         </div>
         <div id="cardBack" class="absolute w-full h-full bg-white flex flex-col p-6 items-start items-center justify-center justify-between backface-hidden rotate-y-180">
             <p class="font-sans text-lg font-light text-black text-center">
-                {{ $word ? $word->korean_definition : '기본값' }}
+                {{ $word ? $word->korean_definition : '定義ディフォルト' }}
             </p>
             <button id="unflipButton" class="mt-auto px-4 py-2 bg-blue-500 text-white rounded-lg">
                 戻り
